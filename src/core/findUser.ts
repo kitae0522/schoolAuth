2b66dbd27604060f3bb5f7ed7e587a47f21f63a2
GalaKrond <74582518+GalaKrond-jkr0404@users.noreply.github.com>
2022-04-05 01:03:56 +0900

    Update findUser.ts

--- src/core/findUser.ts
+++ src/core/findUser.ts
@@ -1,23 +1,23 @@
 import axios from 'axios';
-import RSA_ENC from './cryptoHandle';
-import { findUserType } from '../types';
+import RSA_ENC from './cryptoHandle'; // 암호화 
+import { findUserType } from '../types'; // 필수요소 확인
 
-const findUserAPIHandle = (authURL: string): string => (`https://${authURL}/v2/findUser`)
+const findUserAPIHandle = (authURL: string): string => (`https://${authURL}/v2/findUser`) 
 
 const findUser = async (authURL: string, schoolCode: string, userName: string, userBirthday: string):
     Promise<findUserType> => {
 
     if (!authURL || !schoolCode || !userName || !userBirthday)
-        throw new Error('필수 옵션이 비어있습니다.');
+        throw new Error('필수 옵션이 비어있습니다.'); // 입력값 4개 중 하나라도 존재하지 않을 경우 에러 뱉기
 
     try {
         await axios.post(findUserAPIHandle(authURL),
             JSON.stringify({
-                name: RSA_ENC(userName),
-                birthday: RSA_ENC(userBirthday),
+                name: RSA_ENC(userName), // 이름 RSA 암호화해서 넣어두기
+                birthday: RSA_ENC(userBirthday), // 생일도
                 loginType: 'school',
                 orgCode: schoolCode
-            }),
+            }), // 를 JSON화해두기
             { headers: { 'Content-Type': 'application/json; charset=utf-8' } });
         return {
             isError: false,
